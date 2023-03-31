56c4b9d85155aa782d8bde0bc4928380018e6258
Rohan Palankar <rohanpalankar8@gmail.com>
2023-03-31 12:17:52 +0530

    modify files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 
 import './App.css';
+import { useDispatch } from 'react-redux';
+import { buyCar, saleCar } from './actions';
+
+
+
 
 function App() {
+
+  const dispatch = useDispatch();
+
+  const handleBuyCar = () => {
+    dispatch(buyCar());
+  }
+
+  const handleSaleCar = () => {
+    dispatch(saleCar());
+  }
+
   return (
-    <div>
-      Redux
-    </div>
+      <div className='App'>
+          <button onClick={handleBuyCar}>Buy Car</button>
+          <button onClick={handleSaleCar}>Sale Car</button>
+      </div>
   );
 }
 
